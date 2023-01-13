28531af03f1690d363f52de32bfeece51ff3dc46
Paola Garcia <lizpaogarcia2@gmail.com>
2023-01-13 13:17:45 -0300

    Actualizo

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -5,13 +5,14 @@ import { Helmet } from 'react-helmet';
 class PersonCard extends Component {
     render() { 
         const { firstName, lastName, age, hairColor } = this.props;
+        
         return (
             <>
                 <Helmet>
                     <title>Page 1</title>
                 </Helmet>
                 <div>
-                    <h1>{ firstName } { lastName }</h1>
+                    <h1> { lastName }, { firstName }</h1>
                     <p>Age: { age }</p>
                     <p>Hair Color: { hairColor }</p>
                 </div>   
